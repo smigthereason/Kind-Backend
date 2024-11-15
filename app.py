a4ec6903bfef3c2bfd85fad622c5b4c11d3d36c0
Victor Maina <148073062+smigthereason@users.noreply.github.com>
2024-11-16 01:07:53 +0300

    Update app.py allow vercel requests

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+CORS(app, resources={r"/*": {"origins": "https://kindr-2.vercel.app"}}, supports_credentials=True)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 CORS(app, resources={r"/charity": {"origins": "http://localhost:5173"}})
